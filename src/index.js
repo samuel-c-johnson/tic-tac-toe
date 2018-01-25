fd3feb30fdcc68c84bf845153108db595fb74342
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-25 12:43:19 +0000

    Adds alert when clicking on a square

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
@@ -15,7 +15,8 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value = {i} />;
+
   }
 
   render() {
